c29bd0f63b7ea0ec942c36f84b054fde910a05b8
Denismaka <makadenis370@gmail.com>
2025-04-14 17:57:40 +0100

    Mise en place de la structure HTML responsive avec boostrap

--- index.php
+++ index.php
@@ -87,6 +87,45 @@
     <!-- ############## -->
     <!-- Services-->
     <!-- ############## -->
+    <section class="services" id="services">
+        <h2 class="text-uppercase fw-bold">services</h2>
+        <div class="container">
+            <div class="row">
+                <div class="card col-md-2 text-center">
+                    <a href="#">
+                        <i class="fas fa-wrench"></i>
+                        <p>ingénierie mécanique</p>
+                    </a>
+                </div>
+                <div class="card col-md-2 text-center">
+                    <a href="#">
+                        <i class="fas fa-cogs"></i>
+                        <p>gestion des carburants et du gaz</p>
+                    </a>
+                </div>
+                <div class="card col-md-2 text-center">
+                    <a href="#">
+                        <i class="fas fa-cog"></i>
+                        <p>secteur de l'énergie et de la puissance
+                        </p>
+                    </a>
+                </div>
+                <div class="card col-md-2 text-center">
+                    <a href="#">
+                        <i class="fa-solid fa-oil-well"></i>
+                        <p>raffinerie de pétrole</p>
+                    </a>
+                </div>
+                <div class="card col-md-2 text-center">
+                    <a href="#">
+                        <i class="fa-solid fa-leaf"></i>
+                        <p>énergie écologique et biologique
+                        </p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
 
     <!-- ############## -->
     <!-- Projets-->
