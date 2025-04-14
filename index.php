3aba2b35125de5d876a6a9f62948748a746ba748
Denismaka <makadenis370@gmail.com>
2025-04-14 15:50:43 +0100

    Debut du projet

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/bootstrap-icons/bootstrap-icons.css">
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="/assets/font/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="/assets/swiper/package/swiper-bundle.min.css">
+    <link rel="website icon" href="/assets/images/Logo.png" />
+    <title>Industries</title>
+</head>
+
+<body>
+    <!-- Bouton de redirection -->
+
+    <!-- ############## -->
+    <!-- Header -->
+    <!-- ############## -->
+
+
+    <!-- ##############  -->
+    <!-- Home  -->
+    <!-- ##############  -->
+
+    <!-- ############## -->
+    <!-- About-->
+    <!-- ############## -->
+
+    <!-- ############## -->
+    <!-- Services-->
+    <!-- ############## -->
+
+    <!-- ############## -->
+    <!-- Projets-->
+    <!-- ############## -->
+
+    <!-- ############## -->
+    <!-- Contact-->
+    <!-- ############## -->
+
+    <!-- ############## -->
+    <!-- Footer-->
+    <!-- ############## -->
+
+
+
+
+
+
+    <script src="/assets/scrollreveal/dist/scrollreveal.min.js"></script>
+    <script src="/assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/Js/app.js"></script>
+</body>
+
+</html>
