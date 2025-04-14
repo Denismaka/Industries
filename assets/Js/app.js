eb9914a93b629bfa927c41401b4d9cbd992d6735
Denismaka <makadenis370@gmail.com>
2025-04-14 16:12:44 +0100

    Mise en place du script pour la gestion des effets de la barre de navigation lors du défilement

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -16,3 +16,9 @@ btn.addEventListener("click", () => {
     behavior: "smooth",
   });
 });
+
+// JavaScript pour les effets de la barre de navigation lors du défilement
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
