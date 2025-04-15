47b7631004ee4403a6530df24c2b9e7132c0c8b3
Denismaka <makadenis370@gmail.com>
2025-04-15 09:47:05 +0100

    Ajout des classes boostrap pour la gestion du responsive

--- index.php
+++ index.php
@@ -203,18 +203,22 @@
         <div class="heading">
             <h4 class="text-uppercase fw-bold">contact</h4>
         </div>
-        <form action="#">
-            <div class="input-box">
-                <input type="text" placeholder="Nom" />
-                <input type="text" placeholder="Prenom" />
+        <div class="container">
+            <div class="row">
+                <form action="#">
+                    <div class="input-box">
+                        <input type="text" placeholder="Nom" />
+                        <input type="text" placeholder="Prenom" />
+                    </div>
+                    <div class="input-box">
+                        <input type="number" placeholder="Téléphone" />
+                        <input type="email" placeholder="Email" />
+                    </div>
+                    <textarea placeholder="Message"></textarea>
+                    <input type="submit" value="Envoyer" class="btn" />
+                </form>
             </div>
-            <div class="input-box">
-                <input type="number" placeholder="Téléphone" />
-                <input type="email" placeholder="Email" />
-            </div>
-            <textarea placeholder="Message"></textarea>
-            <input type="submit" value="Envoyer" class="btn" />
-        </form>
+        </div>
     </section>
 
     <!-- ############## -->
