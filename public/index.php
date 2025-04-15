a8abf14d8544af5e7a436718dd91011d13474b87
Denismaka <makadenis370@gmail.com>
2025-04-15 12:03:10 +0100

    Modification des emplacements

--- public/index.php
+++ public/index.php
@@ -21,211 +21,65 @@
     <!-- ############## -->
     <!-- Header -->
     <!-- ############## -->
-    <header>
-        <a href="index.php" class="logo">
-            <img src="/assets/images/logo.png" class="img-fluid rounded" />
-            Industries
-        </a>
-        <div class="menu-toggle" id="mobile-menu">
-            <i class="fas fa-bars"></i>
-        </div>
-        <ul class="nav-list" id="nav-list">
-            <li><a href="/index.php#home" class="text-capitalize">home</a></li>
-            <li><a href="/index.php#about" class="text-capitalize">about</a></li>
-            <li>
-                <a href="/index.php#services" class="text-capitalize">services</a>
-            </li>
-            <li><a href="/index.php#projets" class="text-capitalize">projets</a></li>
-            <li>
-                <a href="/index.php#contact" class="text-capitalize">contact</a>
-            </li>
-        </ul>
-    </header>
+    <?php
+    include '../layout/header.php';
+    ?>
 
 
     <!-- ##############  -->
     <!-- Home  -->
     <!-- ##############  -->
-    <section class="home" id="home">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-8">
-                    <h5>fournit des solutions de <span>construction</span> efficaces.</h5>
-                    <h2>
-                        L'industrialisation est la production de biens ou de services connexes au sein d'une économie, ce qui en fait un indicateur pertinent.
-                    </h2>
-                    <a href="#" class="btn">contact</a>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php
+    include '../views/home.php';
+    ?>
+
 
     <!-- ############## -->
     <!-- About-->
     <!-- ############## -->
-    <section class="about" id="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-6 mt-4">
-                    <div class="thumb">
-                        <a rel="nofollow" href="http://youtube.com" class="play-btn"><i class="fa fa-play"></i></a>
-                        <img src="/assets/images/about.webp" class="img-fluid rounded" alt="">
-                    </div>
-                </div>
-                <div class="col-12 col-md-6 mt-4">
-                    <h4 class="text-uppercase">about</h4>
-                    <h2 class="text-uppercase fw-bold">Bienvenue chez <a href="#" class="text-decoration-none">Industries !</a></h2>
-                    <p>Nous sommes dédiés à fournir des solutions innovantes et efficaces pour la production de biens et de services. Notre mission est de contribuer à l'industrialisation durable, en offrant des services qui répondent aux besoins de l'économie moderne. Grâce à notre expertise et notre engagement, nous nous efforçons d'être un acteur clé dans le développement industriel de notre région. Avec une équipe d'experts passionnés et des technologies de pointe, nous nous engageons à optimiser chaque étape de vos projets. Que ce soit par le biais de la production, de la logistique ou de la gestion de la chaîne d'approvisionnement, nous travaillons main dans la main avec nos clients pour garantir des résultats exceptionnels.
-                    </p>
-                    <a href="#" class="btn">En savoir plus</a>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php
+    include '../views/about.php';
+    ?>
+
 
     <!-- ############## -->
     <!-- Services-->
     <!-- ############## -->
-    <section class="services" id="services">
-        <h2 class="text-uppercase fw-bold">services</h2>
-        <div class="container">
-            <div class="row">
-                <div class="card col-md-2 text-center">
-                    <a href="#">
-                        <i class="fas fa-wrench"></i>
-                        <p>ingénierie mécanique</p>
-                    </a>
-                </div>
-                <div class="card col-md-2 text-center">
-                    <a href="#">
-                        <i class="fas fa-cogs"></i>
-                        <p>gestion des carburants et du gaz</p>
-                    </a>
-                </div>
-                <div class="card col-md-2 text-center">
-                    <a href="#">
-                        <i class="fas fa-cog"></i>
-                        <p>secteur de l'énergie et de la puissance
-                        </p>
-                    </a>
-                </div>
-                <div class="card col-md-2 text-center">
-                    <a href="#">
-                        <i class="fa-solid fa-oil-well"></i>
-                        <p>raffinerie de pétrole</p>
-                    </a>
-                </div>
-                <div class="card col-md-2 text-center">
-                    <a href="#">
-                        <i class="fa-solid fa-leaf"></i>
-                        <p>énergie écologique et biologique
-                        </p>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php
+    include '../views/services.php';
+    ?>
+
 
     <!-- ############## -->
     <!-- Projets-->
     <!-- ############## -->
-    <section class="projets" id="projets">
-        <div class="section-heading text-center">
-            <h2 class="text-uppercase fw-bold">projet d'inspiration</h2>
-            <p class="py-4">Découvrez nos réalisations créatives qui fusionnent innovation et design. Chaque projet raconte une histoire unique, façonnée par notre passion pour l'excellence.</p>
-        </div>
+    <?php
+    include '../views/projets.php';
+    ?>
 
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3">
-                    <img src="/assets/images/projets-01.webp" class="img-fluid rounded" alt="">
-                </div>
-                <div class="col-md-3">
-                    <img src="/assets/images/projets-02.webp" class="img-fluid rounded" alt="">
-                </div>
-                <div class="col-md-3">
-                    <img src="/assets/images/projets-03.webp" class="img-fluid rounded" alt="">
-                </div>
-                <div class="col-md-3">
-                    <img src="/assets/images/projets-04.webp" class="img-fluid rounded" alt="">
-                </div>
-                <div class="col-md-3">
-                    <img src="/assets/images/projets-05.webp" class="img-fluid rounded" alt="">
-                </div>
-                <div class="col-md-3">
-                    <img src="/assets/images/projets-06.webp" class="img-fluid rounded" alt="">
-                </div>
-                <div class="col-md-3">
-                    <img src="/assets/images/projets-07.webp" class="img-fluid rounded" alt="">
-                </div>
-                <div class="col-md-3">
-                    <img src="/assets/images/projets-08.webp" class="img-fluid rounded" alt="">
-                </div>
-            </div>
-        </div>
-    </section>
 
     <!-- ############## -->
     <!-- Newsletter-->
     <!-- ############## -->
-    <section class="newsletter" id="newsletter">
-        <div class="section-heading text-center">
-            <h2 class="text-uppercase fw-bold py-4">newsletter</h2>
-        </div>
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-md-6 text-md-left">
-                    <h2>Restez informé !</h2>
-                    <p>Abonnez-vous à notre newsletter pour être informé en priorité de nos dernières innovations techniques, de nos promotions exclusives et de nos nouveaux produits destinés au secteur de la construction. Ne manquez pas les opportunités de rester à la pointe de votre métier !</p>
-                </div>
-                <div class="col-md-6">
-                    <form class="d-flex">
-                        <div class="form-group flex-grow-1">
-                            <label for="email">Votre adresse e-mail :</label>
-                            <div class="input-group">
-                                <input type="email" class="form-control" id="email" placeholder="Entrez votre e-mail" required>
-                                <div class="input-group-append">
-                                    <button type="submit" class="btn">S'abonner</button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php
+    include '../views/newsletter.php';
+    ?>
+
 
     <!-- ############## -->
     <!-- Contact-->
     <!-- ############## -->
-    <section class="contact" id="contact">
-        <div class="heading">
-            <h4 class="text-uppercase fw-bold">contact</h4>
-        </div>
-        <div class="container">
-            <div class="row">
-                <form action="#">
-                    <div class="input-box">
-                        <input type="text" placeholder="Nom" />
-                        <input type="text" placeholder="Prenom" />
-                    </div>
-                    <div class="input-box">
-                        <input type="number" placeholder="Téléphone" />
-                        <input type="email" placeholder="Email" />
-                    </div>
-                    <textarea placeholder="Message"></textarea>
-                    <input type="submit" value="Envoyer" class="btn" />
-                </form>
-            </div>
-        </div>
-    </section>
+    <?php
+    include '../views/contact.php';
+    ?>
+
 
     <!-- ############## -->
     <!-- Footer-->
     <!-- ############## -->
-    <footer class="py-5">
-        <p class="text-center mt-4">&copy; 2025 <a href="#" class="text-decoration-none">Denis Maka</a>, Tous droits réservés.</p>
-    </footer>
+    <?php
+    include '../layout/footer.php';
+    ?>
 
 
 
