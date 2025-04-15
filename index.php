3a3b19097542d9459cfb260137f7eb76d2dda58e
Denismaka <makadenis370@gmail.com>
2025-04-15 09:56:49 +0100

    suppression de marge dans le col-md-8 de la section Home

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
     <section class="home" id="home">
         <div class="container">
             <div class="row">
-                <div class="col-12 col-md-8 mx-5">
+                <div class="col-12 col-md-8">
                     <h5>fournit des solutions de <span>construction</span> efficaces.</h5>
                     <h2>
                         L'industrialisation est la production de biens ou de services connexes au sein d'une économie, ce qui en fait un indicateur pertinent.
