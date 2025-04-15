bcfd996a8254127a30b22dff168adc5798eb049e
Denismaka <makadenis370@gmail.com>
2025-04-15 12:53:22 +0100

    Mise en place du scrollReveal

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -22,3 +22,15 @@ window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+ScrollReveal({
+  reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
+});
+ScrollReveal().reveal(".container, .heading", { origin: "top" });
+ScrollReveal().reveal(".col-md-4, form", { origin: "top" });
+ScrollReveal().reveal("h1, h3, h4, h5, h2, span, li", { origin: "top" });
+ScrollReveal().reveal("p, .row", { origin: "top" });
+ScrollReveal().reveal("i, a, strong", { origin: "right" });
