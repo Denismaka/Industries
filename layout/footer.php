56cb67fc2ddb0275b846abe467e2278580d28a5f
Denismaka <makadenis370@gmail.com>
2025-04-15 12:01:08 +0100

    Mise en place du dossier layout

---
+++ layout/footer.php
@@ -0,0 +1,6 @@
+ <!-- ############## -->
+ <!-- Footer-->
+ <!-- ############## -->
+ <footer class="py-5">
+     <p class="text-center mt-4">&copy; 2025 <a href="#" class="text-decoration-none">Denis Maka</a>, Tous droits réservés.</p>
+ </footer>
---
+++ layout/header.php
@@ -0,0 +1,23 @@
+  <!-- ############## -->
+  <!-- Header -->
+  <!-- ############## -->
+  <header>
+      <a href="index.php" class="logo">
+          <img src="/assets/images/logo.png" class="img-fluid rounded" />
+          Industries
+      </a>
+      <div class="menu-toggle" id="mobile-menu">
+          <i class="fas fa-bars"></i>
+      </div>
+      <ul class="nav-list" id="nav-list">
+          <li><a href="/index.php#home" class="text-capitalize">home</a></li>
+          <li><a href="/index.php#about" class="text-capitalize">about</a></li>
+          <li>
+              <a href="/index.php#services" class="text-capitalize">services</a>
+          </li>
+          <li><a href="/index.php#projets" class="text-capitalize">projets</a></li>
+          <li>
+              <a href="/index.php#contact" class="text-capitalize">contact</a>
+          </li>
+      </ul>
+  </header>
